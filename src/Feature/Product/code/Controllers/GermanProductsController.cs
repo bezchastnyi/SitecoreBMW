d0e77f935ad158c6bcc9ab9887253f70b4ae475f
Alexei Bezchastnyi <alekseybezchastnyy9@gmail.com>
2021-11-05 21:01:18 +0200

    fix site bugs

---
+++ src/Feature/Product/code/Controllers/GermanProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Sitecore.Data.Items;
+
+namespace Feature.Product.Controllers
+{
+  public class GermanProductsController : Controller
+  {
+    public ActionResult Index()
+    {
+      var allProductsItem = Sitecore.Context.Database.Items.GetItem("/sitecore/Content/BMW/All Products");
+      var products = new List<Item>(allProductsItem.Children);
+      var germanProducts = new List<Item>();
+
+      foreach (var product in products)
+      {
+        if (product.Fields["Country"].Value.Contains("Germany"))
+        {
+          germanProducts.Add(product);
+        }
+      }
+
+      return View(germanProducts.OrderByDescending(x => x.Fields["Year"].Value).ToList());
+    }
+  }
+}
